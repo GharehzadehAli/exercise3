241d65d4304d0e52c1b8292c342c982f58ffaeed
GharehzadehAli <gharehzadehali@gmail.com>
2019-03-29 17:13:33 +0430

    PhoneCall implemented

--- PhoneCall.java
+++ PhoneCall.java
@@ -1,4 +1,19 @@
 package com.company;
 
-public class PhoneCall {
+public abstract class PhoneCall {
+    private String phoneNumber;
+    private double price;
+    PhoneCall(String phoneNumber){
+        this.price=0.0;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public abstract double getPrice() ;
+
+    public abstract String getPhoneNumber();
+
+    public abstract String getInformation();
 }
